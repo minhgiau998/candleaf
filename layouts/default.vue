0c1dcc62dac887150117e6347c93a5bca17de5a4
Rich Nguyen <minhgiau04041998@gmail.com>
2021-11-13 09:25:40 +0700

    init project

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <base-nav-bar></base-nav-bar>
+    <Nuxt />
+    <base-footer></base-footer>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
