951abe5a11df61e1f5b000813f45a9760621fcbc
Rich Nguyen <minhgiau04041998@gmail.com>
2021-11-18 15:10:46 +0700

    Update the home hero component
    - Add api for home hero component
    - Change the name of background image

--- repositories/Repository.js
+++ repositories/Repository.js
@@ -1,5 +1,7 @@
+import HeroRepository from '~/repositories/HeroRepository'
 import ProductsRepository from '~/repositories/ProductsRepository'
 
 export default ($axios) => ({
+  hero: HeroRepository($axios),
   products: ProductsRepository($axios),
 })
